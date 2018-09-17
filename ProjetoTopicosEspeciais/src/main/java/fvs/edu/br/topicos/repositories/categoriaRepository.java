6df742e06e58ad6f1fdd58dc6ca2915d8fe1ccc7
robervalG <roberval@gmail.com>
2018-09-17 20:00:53 -0300

    implementação da camada de persistenncia

---
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/repositories/categoriaRepository.java
@@ -0,0 +1,8 @@
+package fvs.edu.br.topicos.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class categoriaRepository 
+extends JpaRepository<Categoria, Integer>{
+}
