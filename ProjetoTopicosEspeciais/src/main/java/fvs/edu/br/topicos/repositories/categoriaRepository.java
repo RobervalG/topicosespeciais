0988191f7c53e80a98d1bda611bc2b47522a7eb2
robervalG <roberval@gmail.com>
2018-09-18 20:07:45 -0300

    Im da camada de serviços de categoria

--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/repositories/categoriaRepository.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/repositories/categoriaRepository.java
@@ -1,8 +1,15 @@
+
 package fvs.edu.br.topicos.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import fvs.edu.br.topicos.domain.Categoria;
+
 @Repository
-public class categoriaRepository 
-extends JpaRepository<Categoria, Integer>{
+public interface categoriaRepository extends JpaRepository<Categoria, Integer>{
+
+	
+	
 }
+
--- ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/resources/categoriaResourse.java
+++ ProjetoTopicosEspeciais/src/main/java/fvs/edu/br/topicos/resources/categoriaResourse.java
@@ -1,28 +1,27 @@
 package fvs.edu.br.topicos.resources;
 
-import java.util.List;
-import java.util.ArrayList;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import fvs.edu.br.topicos.domain.Categoria;
+import fvs.edu.br.topicos.service.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
-public class CategoriaResourse {
+public class categoriaResourse {
+	
+	@Autowired
+	private CategoriaService service;	
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Bombril");
-		Categoria cat2 = new Categoria(2, "Detergente");
+	public ResposeEntity<?> listar(@PathVariable Integer id){
+	Categoria obj = service.Buscat(Id);
+	
 		
-		List<Categoria> lista = new ArrayList <>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+		return ResponseEntity.ok(obj);
 				
 	}
 
